b2d2a085d70ddae063f08e7d9b7395785a75c60a
gurinder725 <149006719+gurinder725@users.noreply.github.com>
2023-10-25 18:52:27 +0000

    varibles console.table

---
+++ Basic/variables.js
@@ -0,0 +1,12 @@
+const names = "Gurinder"
+let lastName = "Singh"
+var surName = "Gill"
+let rollNumber = 123
+
+
+//names = "javascript"
+lastName = "basics"
+rollNumber = 111
+
+console.table([names, lastName, rollNumber])
+
